4bf663ce33225ee2626a7afbacb69a361e4ae59e
Jan on t530 <jan.debiec747@gmail.com>
2017-04-29 07:50:06 +0200

    cleaning code

--- source/top/c4_1702.vhd
+++ source/top/c4_1702.vhd
@@ -178,13 +178,6 @@ sl_clk50MHz <= CLOCK_50;
 sl_Reset <= not (KEY(0));
     sl_SpiClk <= isl_SpiClk;
 
---GPIO_2(1) <= sl_slice_tick;
---GPIO_2(2) <= sl_output1A;
---GPIO_2(3) <= sl_output1B;
---GPIO_2(4) <= sl_output2A;
---GPIO_2(5) <= sl_output2B;
---GPIO_2(6) <= sl_step;
---GPIO_2(7) <= sl_PwmPeriodPulse;
 
     osl_outL1A  <= uAxisL_sl_output1A;
     osl_outL1B  <= uAxisL_sl_output1B;
@@ -236,14 +229,14 @@ end process;
 
 
 
-U_PwmPulseGen : pwm_pulse
-port map
-(
-	isl_clk50Mhz 		=> sl_clk50MHz,--: in std_logic;	--!
-	isl_rst 			=> sl_Reset,--: in std_logic;	--!
-	osl_PwmPeriodPulse 	=> sl_PwmPeriodPulse,--: in std_logic;
-	ou16_loopCounter	=> u16_loopCounter--: out integer	--!
-);
+--U_PwmPulseGen : pwm_pulse
+--port map
+--(
+--	isl_clk50Mhz 		=> sl_clk50MHz,--: in std_logic;	--!
+--	isl_rst 			=> sl_Reset,--: in std_logic;	--!
+--	osl_PwmPeriodPulse 	=> sl_PwmPeriodPulse,--: in std_logic;
+--	ou16_loopCounter	=> u16_loopCounter--: out integer	--!
+--);
 
 --!
 uST : slice_tick_gen
@@ -280,65 +273,9 @@ begin
 end generate;   
 
 
---U_convVel2Pulse : convVel2Pulse
---port map
---(
---	isl_clk50Mhz 		=> sl_clk50MHz,--: in std_logic;
---	isl_rst 			=> sl_Reset,--: in std_logic;
---	isl_sliceTick 		=> uST_sl_sliceTick,--in std_logic; --! 50 ms tick for velocity changes
---	in16_inputVector 	=> n16_inputVector,--in signed (15 downto 0);--! input velocity 15 bits + sign
---	in16_rampValue  	=> n16_rampValue,--in signed (15 downto 0);--! ramp, allowed changes of velocity per tick
-----	oslv16_testValue  	=> ,--out std_logic_vector (15 downto 0);--! used for tesing
---	osl_pulse			=> sl_step--out std_logic--! used for tesing
---);
-
-
-
---U_convPulse2Pos : convPulse2Pos
---port map
---(
---	isl_clk50Mhz 		=> sl_clk50MHz,--: in std_logic;
---	isl_rst 			=> sl_Reset,--: in std_logic;
---	isl_direction		=> sl_direction,--: in std_logic;
---	isl_pulse			=> sl_step,--: in std_logic;
---	oslv6_OutputValue  	=> slv6_PosModulo--: out std_logic_vector (5 downto 0)--! 
---);
-
---U_POSITION_JTAG : if (bISSP = TRUE and bModelSim = FALSE) generate
---begin
---	U_POSITION_ISSP : entity work.position_issp
---	generic map(
-----		bISSP     =>TRUE,
---		bModelSim => FALSE
---	)
---	port map (
---		isl_clk50Mhz 		=> sl_clk50Mhz,--: in std_logic;
---		isl_rst 			=> sl_Reset,--: in std_logic;
---		islv6_inputPosition 	=> slv6_PosModulo,--: in signed (15 downto 0);
---		oslv6_outputPosition 	=> slv6_InputIndexIssp--: out signed (15 downto 0);
---	);
---end generate;
---
---
---slv6_InputIndexModulo <= slv6_InputIndexIssp when (bISSP = TRUE and bModelSim = FALSE) else
---				slv6_PosModulo;
-
 slv4_switch <= SW;
 sl_configSpiOrUart <= slv4_switch(0);
---!
---U_SingleAxis : convPos2Pwm
---port map
---(
---	isl_clk50Mhz 		=> sl_clk50MHz,--: in std_logic;
---	isl_rst 			=> sl_Reset,--: in std_logic;
---	islv6_InputIndex 	=> slv6_InputIndexModulo,--: in std_logic_vector(7 downto 0);
---	iu16_loopCounter 	=> u16_loopCounter,--: in integer;
---	isl_InputSync		=> sl_PwmPeriodPulse,--: in std_logic;
---	osl_output1A		=> sl_output1A ,--	: out std_logic;
---	osl_output1B		=> sl_output1B ,--	: out std_logic;
---	osl_output2A		=> sl_output2A ,--	: out std_logic;
---	osl_output2B		=> sl_output2B --	: out std_logic
---);
+
 pInputL : process (
    all 
 )begin
@@ -374,8 +311,6 @@ pInputR : process (
         end if;
     END IF;
 end process;
-
-
 
 uM : monoshot 
 port map     (
