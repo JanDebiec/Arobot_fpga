fe02663a4cb69fa2241043545bbd04380c3ae59c
jan@t460-U16 <jan.debiec@ue-mail.de>
2017-07-03 11:12:16 +0200

    implementing output

--- source/spi/spi_output.vhd
+++ source/spi/spi_output.vhd
@@ -65,8 +65,10 @@ entity spi_output is
 -- system side
     isl_SystemClock  : in STD_LOGIC ;
     isl_reset        : in std_logic;
-    islv8_status     : STD_LOGIC_VECTOR (7 DOWNTO 0);
-    islv8_command    : STD_LOGIC_VECTOR (7 DOWNTO 0)
+    islv8_MagicWord  : in std_logic_vector(7 downto 0);
+    islv8_Header     : in std_logic_vector(7 downto 0);
+    islv32_DataL     : in std_logic_vector(31 downto 0);
+    islv32_DataR     : in std_logic_vector(31 downto 0);
  --   islv11_OutWrAddr : in STD_LOGIC_VECTOR (10 DOWNTO 0);
 --    isl_SysClkEna    : in STD_LOGIC;
 --    isl_OutWrEna     : in STD_LOGIC;
@@ -88,16 +90,24 @@ architecture RTL of spi_output is
     signal sl_SpiClk : std_logic;
     --signal sl_TxActive : std_logic;
     signal sl_validData : std_logic;
-    signal slv8_Data : std_logic_vector(7 downto 0);
+    signal uTxPrep_slv8_Data : std_logic_vector(7 downto 0);
     signal uTx_sl_dataReqSpi : std_logic;
+    signal uTxB_sl_dataReqSpi : std_logic;
     signal sl_dataReqSpiDel : std_logic;
     signal sl_dataReqSpi2Del : std_logic;
     
     signal sl_dataReqSys : std_logic;
     signal uTx_sl_TxReady : std_logic;
+    signal uTxB_sl_TxReady : std_logic;
     signal uTx_sl_miso : std_logic;
     signal uM_sl_TxActive_m : std_logic;
     
+    signal uTxPrep_sl_firstByteValid : std_logic;
+    signal uTxPrep_sl_DataValid : std_logic;
+    signal uTxPrep_sl_txActive : std_logic;
+    signal uTxPrepB_sl_firstByteValid : std_logic;
+    signal uTxPrepB_sl_DataValid : std_logic;
+    signal uTxPrepB_sl_txActive : std_logic;
 ---- signals for DpRam
 ---- read side    
 --    signal slv8_RdData : STD_LOGIC_VECTOR (7 DOWNTO 0);
@@ -120,7 +130,7 @@ begin
     sl_Reset <= isl_Reset;
     --sl_TxActive <= isl_TxActive;
     
-    slv8_Data <= islv8_status;-- when (u16_RdAddrCnt(0) = '0') else
+--    uTxPrep_slv8_Data <= islv8_status;-- when (u16_RdAddrCnt(0) = '0') else
                 --islv8_command;
     sl_validData <= uTx_sl_dataReqSpi;--uM_sl_TxActive_m;-- or sl_dataReqSpi2Del;
     
@@ -181,14 +191,83 @@ port map
 --! signals on OutputCLock domain
     isl_SpiClk       => sl_SpiClk,
     isl_reset        => sl_Reset,--: in std_logic;
-    isl_TxActive     => isl_TxActive,--: in std_logic;
-    isl_FirstByteValid => uM_sl_TxActive_m,
-    isl_validData    => sl_validData,--: in std_logic;
-    islv8_Data       => slv8_Data,--: in std_logic_vector(7 downto 0);
+    isl_TxActive     => uTxPrepB_sl_txActive,--: in std_logic;
+    isl_FirstByteValid => uTxPrepB_sl_firstByteValid,
+    isl_validData    => uTxPrepB_sl_DataValid,--: in std_logic;
+    islv8_Data       => uTxPrep_slv8_Data,--: in std_logic_vector(7 downto 0);
     osl_dataReq      => uTx_sl_dataReqSpi,--
     osl_TxReady      => uTx_sl_TxReady,--
     osl_miso         => uTx_sl_miso--: out std_logic := 'Z'
 );
+
+uTxPrep : spi_output_prepare
+port map
+(
+-- system side
+    isl_SystemClock  => sl_SystemClock,--: in STD_LOGIC ;
+    islv8_MagicWord  => islv8_MagicWord  ,--: in std_logic_vector(7 downto 0);
+    islv8_Header     => islv8_Header     ,--: in std_logic_vector(7 downto 0);
+    islv32_DataL     => islv32_DataL     ,--: in std_logic_vector(31 downto 0);
+    islv32_DataR     => islv32_DataR     ,--: in std_logic_vector(31 downto 0);
+    isl_transferData => isl_TxActive ,--: in std_logic;
+    isl_trDataReq    => uTxB_sl_dataReqSpi,
+    isl_trReady      => uTxB_sl_TxReady,
+    oslv8_outData    => uTxPrep_slv8_Data    ,--: out std_logic_vector(7 downto 0);
+    osl_firstByteValid  => uTxPrep_sl_firstByteValid,--: out std_logic;
+    osl_DataValid    => uTxPrep_sl_DataValid,--: out std_logic;
+    osl_txActive     => uTxPrep_sl_txActive,--: out std_logic;
+    isl_reset        => sl_reset--: in std_logic;
+);
+
+uTxDataReq : mono_on_border
+    PORT map
+    (
+    i_InputClock    => sl_SpiClk,--: in  STD_LOGIC;--
+    i_OutputClock   => sl_SystemClock,--: in  STD_LOGIC;--
+    i_Input         => uTx_sl_dataReqSpi,--: in  STD_LOGIC;--
+    i_Reset         => sl_reset,--: in  STD_LOGIC;--
+    o_Output        => uTxB_sl_dataReqSpi--: out STD_LOGIC     --
+    );
+
+uTxDataReady : mono_on_border
+    PORT map
+    (
+    i_InputClock    => sl_SpiClk,--: in  STD_LOGIC;--
+    i_OutputClock   => sl_SystemClock,--: in  STD_LOGIC;--
+    i_Input         => uTx_sl_TxReady,--: in  STD_LOGIC;--
+    i_Reset         => sl_reset,--: in  STD_LOGIC;--
+    o_Output        => uTxB_sl_TxReady--: out STD_LOGIC     --
+    );
+
+uFirstByteVal : mono_on_border
+    PORT map
+    (
+    i_InputClock    => sl_SystemClock,--: in  STD_LOGIC;--
+    i_OutputClock   => sl_SpiClk,--: in  STD_LOGIC;--
+    i_Input         => uTxPrep_sl_firstByteValid,--: in  STD_LOGIC;--
+    i_Reset         => sl_reset,--: in  STD_LOGIC;--
+    o_Output        => uTxPrepB_sl_firstByteValid--: out STD_LOGIC     --
+    );
+
+uTxDataVal : mono_on_border
+    PORT map
+    (
+    i_InputClock    => sl_SystemClock,--: in  STD_LOGIC;--
+    i_OutputClock   => sl_SpiClk,--: in  STD_LOGIC;--
+    i_Input         => uTxPrep_sl_DataValid,--: in  STD_LOGIC;--
+    i_Reset         => sl_reset,--: in  STD_LOGIC;--
+    o_Output        => uTxPrepB_sl_DataValid--: out STD_LOGIC     --
+    );
+
+uTxActive : mono_on_border
+    PORT map
+    (
+    i_InputClock    => sl_SystemClock,--: in  STD_LOGIC;--
+    i_OutputClock   => sl_SpiClk,--: in  STD_LOGIC;--
+    i_Input         => uTxPrep_sl_txActive,--: in  STD_LOGIC;--
+    i_Reset         => sl_reset,--: in  STD_LOGIC;--
+    o_Output        => uTxPrepB_sl_txActive--: out STD_LOGIC     --
+    );
 
 -- write in system side
 --read on spi side
